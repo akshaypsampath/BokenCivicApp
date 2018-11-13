c8401b498664e8ddd66b5728aa3616424ffd9e40
Akshay Sampath <akshaysampath@Akshays-MacBook-Pro.local>
2018-11-13 15:18:27 -0500

    react-native-local-mongodb installed

--- App.js
+++ App.js
@@ -1,15 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Component, TouchableOpacity, Image} from 'react-native';
 
-export default class App extends React.Component {
+
+export default class ButtonBasics extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+_onPressButtonAlt = () => {
+    Alert.alert('you\'re different'+this.state.count)
+    this.setState({ count: this.state.count+1 })
+    if(this.state.count==4)
+        this.setState({ count: 0 })
+
+  }
+  _doNothing() {
+
+  }
+
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButtonAlt}
+            title="This looks great!"
+          />
+          <TouchableOpacity onPress={this._onPressButtonAlt} onLongPress={this._onPressButton}>
+          <Image source={pic} style={{width: 250, height: 160}}/>
+          {/*<Button
+            onPress={this._doNothing}
+            title="HI"
+            color="#841584"
+          />*/}
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
